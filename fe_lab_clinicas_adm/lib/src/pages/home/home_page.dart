b02b72c26cad76e6f9a645c179d4bcb301e2f629
Ruben Manuel <rubensapalo97@gmail.com>
2024-02-28 18:00:28 +0100

    Criacao da tela home

--- fe_lab_clinicas_adm/lib/src/pages/home/home_page.dart
+++ fe_lab_clinicas_adm/lib/src/pages/home/home_page.dart
@@ -1,20 +1,67 @@
+import 'dart:ffi';
+
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:validatorless/validatorless.dart';
 
 class HomePage extends StatefulWidget {
-
-  const HomePage({ super.key });
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  
+  final formKey = GlobalKey<FormState>();
+  final deskNumber = TextEditingController();
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: LabClinicasAppBar(),
+      body: Center(
+          child: Form(
+            child: Container(
+                    padding: const EdgeInsets.all(40),
+                    width: mediaQuery.width * .50,
+                    decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: LabClinicasTheme.orangeColor)),
+                    child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Bem-vindo!', style: LabClinicasTheme.titleStyle),
+              const SizedBox(height: 32),
+              Text('Preencha o número do guiché que você está atendendo',
+                  style: LabClinicasTheme.subTitleSmallStyle),
+                const SizedBox(height: 24),
+                SizedBox(
+                 width: double.infinity,
+                  child: TextFormField(
+                    controller: deskNumber,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    validator: Validatorless.multiple(
+                      [
+                        Validatorless.required('Guichê obrigatorio'),
+                        Validatorless.number('Guichê deve ser um número')
+                      ]
+                    ),
+                    decoration: InputDecoration(label: Text('Número do Guichê')),
+                  )),
+                 const SizedBox(height: 32),
+                SizedBox(
+                  height: 48,
+                  width: double.infinity,
+                  child: ElevatedButton(onPressed: (){}, child: const Text('CHAMAR PRÓXIMO PACIENTE')))
+              
+            ],
+                    ),
+                  ),
+          )),
+    );
   }
 }
