1d27df27b84787f4c4aba230407b0ebb37261085
Ruben Manuel <rubensapalo97@gmail.com>
2024-03-14 16:29:41 +0100

    Modulo DeskTop

--- fe_lab_clinicas_adm/lib/src/pages/end_checkin/end_checkin_page.dart
+++ fe_lab_clinicas_adm/lib/src/pages/end_checkin/end_checkin_page.dart
@@ -1,14 +1,66 @@
+import 'package:fe_lab_clinicas_adm/src/pages/end_checkin/end_checkin_controller.dart';
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:signals_flutter/signals_flutter.dart';
 
-class EndCheckinPage extends StatelessWidget {
+class EndCheckinPage extends StatefulWidget {
 
   const EndCheckinPage({ super.key });
 
+  @override
+  State<EndCheckinPage> createState() => _EndCheckinPageState();
+}
+
+class _EndCheckinPageState extends State<EndCheckinPage> with MessageViewMixin{
+
+  final controller = Injector.get<EndCheckinController>();
+
    @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('End Checkin'),),
-           body: Container(),
-       );
+   void initState() {
+    messageListener(controller);
+    effect((){
+      if(controller.informationForm() != null){
+        Navigator.of(context).pushReplacementNamed('/pre-checkin', arguments: controller.informationForm());
+      }
+    });
+     super.initState();
+   }
+
+    @override
+  Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: LabClinicasAppBar(),
+      body: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+                  margin: const EdgeInsets.only(top: 56),
+                  padding: const EdgeInsets.all(40),
+                  width: mediaQuery.width * .50,
+                  decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(color: LabClinicasTheme.orangeColor)),
+                  child:  Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/check_icon.png'),
+            const SizedBox(height: 40),
+            Text('Atendimento finalizado com sucesso', style: LabClinicasTheme.titleSmalltyle),
+            const SizedBox(height: 40),
+            SizedBox(
+              width: double.infinity,
+              height: 48,
+              child: ElevatedButton(
+                onPressed: (){
+                  controller.callNextPatient();
+                },
+                child: const Text('CHAMAR OUTRA SENHA'),),
+            )
+          ],
+                  ),
+                )),
+    );
   }
 }
